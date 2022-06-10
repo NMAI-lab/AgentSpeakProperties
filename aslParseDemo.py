b74aebe04b2f73fdc8ef56387bc9edee5712b29e
pgavigan <pgavigan@gmail.com>
2022-06-10 12:51:06 -0400

    learning some more yacc

--- aslParseDemo.py
+++ aslParseDemo.py
@@ -16,30 +16,48 @@ rawInput = '+trigger(TriggerValue) : context(ContextValue) <- action(3).'
  
 # List of token names.   This is always required
 tokens = (
-  'NUMBER',
-  'PLUS',
-  'MINUS',
-  'TIMES',
-  'DIVIDE',
-  'LPAREN',
-  'RPAREN',
+  'number',
+  'plus',
+  'minus',
+  'times',
+  'divide',
+  'parenLeft',
+  'parenRight',
+  'bracketLeft',
+  'bracketRight',
+  'exclamation',
+  'colon',
+  'period',
+  'arrow',
+  'implication',
+  'lettersLower',
+  'lettersUpper',
 )
  
 def MyLexer():
     # Regular expression rules for simple tokens
-    t_PLUS    = r'\+'
-    t_MINUS   = r'-'
-    t_TIMES   = r'\*'
-    t_DIVIDE  = r'/'
-    t_LPAREN  = r'\('
-    t_RPAREN  = r'\)'
- 
+    t_plus = r'\+'
+    t_minus = r'-'
+    t_times = r'\*'
+    t_divide = r'\/'
+    t_parenLeft = r'\('
+    t_parenRight = r'\)'
+    t_bracketLeft = r'\['
+    t_bracketRight = r'\]'
+    t_exclamation = r'\!'
+    t_colon = r'\:'
+    t_period= r'\.'
+    t_arrow = r'\<-'
+    t_implication = r'\:-'
+    t_lettersLower = r'[a-z]+'
+    t_lettersUpper = r'[A-Z]+'
+
     # A regular expression rule with some action code
-    def t_NUMBER(t):
+    def t_number(t):
         r'\d+'
         t.value = int(t.value)    
         return t
- 
+
     # Define a rule so we can track line numbers
     def t_newline(t):
         r'\n+'
@@ -58,3 +76,63 @@ def MyLexer():
 
 
 lexer = MyLexer()
+#lexer.input(rawInput)
+#for tok in iter(lex.token, None):
+#    print(repr(tok.type), repr(tok.value))
+    
+    
+#class Plan(object):
+#    def __init__(self, symbol, count):
+#        self.symbol = symbol
+#        self.count = count
+#   def __repr__(self):
+#        return "Plan(%r, %r)" % (self.symbol, self.count)
+
+#def p_term_div(p):
+#    'term : term divide factor'
+#    p[0] = p[1] / p[3]
+    
+#def p_factor_num(p):
+#    'factor : number'
+#    p[0] = p[1]
+    
+#def p_term_factor(p):
+#     'term : factor'
+#     p[0] = p[1]
+     
+def p_term_lettersLower(p):
+    'term : lettersLower'
+    p[0] = str(p[1])
+
+def p_term_lettersUpper(p):
+    'term : lettersUpper'
+    p[0] = str(p[1])
+    
+        
+def p_error(p):
+    raise TypeError("unknown text at %r" % (p.value,))
+    
+#def p_word_letterLower(p):
+#    'word : t_letterLower'
+#    p[0] = str(p[1])
+         
+#def p_binary_operators(p):
+#    '''expression : expression '+' term
+#                  | expression '-' term
+#       term       : term '*' factor
+#                  | term '/' factor'''
+#    if p[2] == '+':
+#        p[0] = p[1] + p[3]
+#    elif p[2] == '-':
+#        p[0] = p[1] - p[3]
+#    elif p[2] == '*':
+#        p[0] = p[1] * p[3]
+#    elif p[2] == '/':
+#        p[0] = p[1] / p[3]
+        
+    
+    
+yacc.yacc()
+result = yacc.parse("ab")
+print(result)
+
