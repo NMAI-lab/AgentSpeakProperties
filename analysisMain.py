ea98cf1a864d9ce354055d9a8d5bf06006f19414
pgavigan <pgavigan@gmail.com>
2022-06-21 09:29:38 -0400

    Need to make a pretty report output

--- analysisMain.py
+++ analysisMain.py
@@ -8,7 +8,7 @@ Created on Tue Jun  7 16:15:23 2022
 from AgentAnalyzer import AgentAnalyzer
 
 def analyze():
-    aib = ['C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/AgentInABox/steeringController.asl',
+    files = ['C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/AgentInABox/steeringController.asl',
                   'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/AgentInABox/carController.asl',
                   'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/AgentInABox/a_star.asl',
                   'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/AgentInABox/driveTowardController.asl',
@@ -18,7 +18,30 @@ def analyze():
                   'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/AgentInABox/speedController.asl']
     agentType = 'idiomatic'
     connectionBias = ['Prioritization']
-    analyzer = AgentAnalyzer(aib, agentType, connectionBias)
+    analyzer = AgentAnalyzer(files, agentType, connectionBias)
+    analyzer.analyze()
+    
+    
+    files = ['C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaGoalDirected/carController.asl',
+            'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaGoalDirected/driveTowardController.asl',
+            'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaGoalDirected/map.asl',
+            'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaGoalDirected/positioningRules.asl',
+            'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaGoalDirected/speedController.asl',
+            'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaGoalDirected/steeringController.asl']
+    agentType = 'goal'
+    connectionBias = []
+    analyzer = AgentAnalyzer(files, agentType, connectionBias)
+    analyzer.analyze()
+    
+    
+    files = ['C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaReactive/carController.asl',
+             'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaReactive/driveTowardController.asl',
+             'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaReactive/map.asl',
+             'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaReactive/positioningRules.asl',
+             'C:/Users/Patrick/Documents/GitHub/AirSimNavigatingCar/asl/vanillaReactive/steeringController.asl']
+    agentType = 'reactive'
+    connectionBias = []
+    analyzer = AgentAnalyzer(files, agentType, connectionBias)
     analyzer.analyze()
     
     
